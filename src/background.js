ef9f56c86c01414e1779b932af87b51d8d9b7332
rarar <me@rarar.com>
2021-04-20 17:49:27 -0700

    updated the dang banner bar

--- src/background.js
+++ src/background.js
@@ -55,15 +55,15 @@ function computeRunningAverage(score) {
   }, ()=> {
     // TODO: pass currentVibe and score to the inContent function
     chrome.tabs.executeScript({
-      code: `(${ inContent })()`
+      code: `(${ inContent.toString() })(${score}, ${avg})`
     }, ()=> {
 
     });
   });
 }
 
-function inContent() {
+function inContent(score, avg) {
   let el = document.body.appendChild(document.createElement('div'));
   el.style.cssText = 'position:fixed; bottom:0; left:0; right:0; background:pink; text-align: center; font-size: 18px; font-family: Helvetica, sans-serif; padding: 16px;';
-  el.innerHTML = 'The vibe of this page is chill';
+  el.innerHTML = 'The vibe of this page is chill. Score is ' + score + ". Average is " + avg;
 }
